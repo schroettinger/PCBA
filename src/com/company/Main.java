ff5e9aa74a816f377d6b9ab5a854c72ed5b12ee2
schroettinger <scheis.provider@web.de>
2015-04-08 18:47:09 +0200

    Initial commit with working frm_main.

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        frm_main Frm_main = new frm_main();
+    }
+}
---
+++ src/com/company/frm_main.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by Daniel Schroeder on 08.04.2015.
+ */
+public class frm_main {
+}
